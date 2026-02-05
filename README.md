a08138b09dbeea0959d69ea7d5e9c941218aae78
jamsamjam <andsunlit@gmail.com>
2026-02-06 00:59:10 +0100

    comment on first.cc

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 https://www.nsnam.org/docs/release/3.46/tutorial/html/
-Download the latest release from https://www.nsnam.org/releases/latest
+https://www.nsnam.org/releases/latest
+
+### Basic Concepts
 
 - `Node`: computing device that connects to a network (like a host or endpoint)
 - `Application`: a user program that generates some activity to be simulated
 - `Channel`: provides methods for managing communication subnetwork objects and connecting nodes to them
 - `NetDevice`: provides methods for managing connections to `Node` and `Channel` objects
 
+### Running Simulations
+
+```shell
+./ns3 configure -d debug --enable-examples --enable-tests
+./ns3 build
+```
+
+---
+
 ### Personal Note
 
-- [] 이부분 compsys 수업에서 했던 프로젝트랑 연결해서 정리해보기
+- [ ] 이부분 compsys 수업에서 했던 프로젝트랑 연결해서 정리해보기
--- experiments/my-first.cc
+++ experiments/my-first.cc
@@ -15,7 +15,7 @@
 //    point-to-point
 //
 
-using namespace ns3;
+using namespace ns3; // we don't have to type ns3:: after this declaration
 
 NS_LOG_COMPONENT_DEFINE("FirstScriptExample");
 
@@ -29,30 +29,40 @@ main(int argc, char* argv[])
     LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
     LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
-    NodeContainer nodes;
+    // 1. Building a point-to-point network
+
+    NodeContainer nodes; // create, manage and access any Node objects
     nodes.Create(2);
+
+    // Recall: NetDevice = peripheral cards, Channel = network cables
 
     PointToPointHelper pointToPoint;
     pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
     pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
 
-    NetDeviceContainer devices;
+    NetDeviceContainer devices; // contains two nodes
     devices = pointToPoint.Install(nodes);
 
-    InternetStackHelper stack;
-    stack.Install(nodes);
+    InternetStackHelper stack; 
+    stack.Install(nodes); // install TCP/IP stack on nodes
 
     Ipv4AddressHelper address;
-    address.SetBase("10.1.1.0", "255.255.255.0");
+    address.SetBase("10.1.1.0", "255.255.255.0"); // first 10.1.1.1, followed by 10.1.1.2
+    // The low level ns-3 system remembers all of the IP addresses allocated
+    // -> a fatal error if the same address is generated twice (hard to debug!)
 
     Ipv4InterfaceContainer interfaces = address.Assign(devices);
 
+    // 2. Applications to generate traffic
+
+    // Set up a UDP echo server application on one of the nodes we have previously created
     UdpEchoServerHelper echoServer(9);
 
-    ApplicationContainer serverApps = echoServer.Install(nodes.Get(1));
-    serverApps.Start(Seconds(1));
+    ApplicationContainer serverApps = echoServer.Install(nodes.Get(1)); // implicit conversion: Ptr<Node> -> NodeContainer
+    serverApps.Start(Seconds(1)); // explicit conversion: double -> Time
     serverApps.Stop(Seconds(10));
 
+    // The first interface in the interfaces container corresponds to the IP address of the first node in the nodes container
     UdpEchoClientHelper echoClient(interfaces.GetAddress(1), 9);
     echoClient.SetAttribute("MaxPackets", UintegerValue(1));
     echoClient.SetAttribute("Interval", TimeValue(Seconds(1)));
@@ -62,6 +72,10 @@ main(int argc, char* argv[])
     clientApps.Start(Seconds(2));
     clientApps.Stop(Seconds(10));
 
+    // 3. Running the simulation
+
+    // The system will begin looking through the list of scheduled events and executing them
+    // Simulator::Stop(Seconds(11));
     Simulator::Run();
     Simulator::Destroy();
     return 0;
