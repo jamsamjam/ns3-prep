8b404e6a5d335b01d05d60092d577f2bd7134127
jamsamjam <andsunlit@gmail.com>
2026-02-06 01:46:48 +0100

    fix symlinks 😭

--- tutorial/first.cc
+++ tutorial/first.cc
@@ -15,7 +15,7 @@
 //    point-to-point
 //
 
-using namespace ns3; // we don't have to type ns3:: after this declaration
+using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("FirstScriptExample");
 
@@ -29,40 +29,30 @@ main(int argc, char* argv[])
     LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
     LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
-    // 1. Building a point-to-point network
-
-    NodeContainer nodes; // create, manage and access any Node objects
+    NodeContainer nodes;
     nodes.Create(2);
-
-    // Recall: NetDevice = peripheral cards, Channel = network cables
 
     PointToPointHelper pointToPoint;
     pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
     pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
 
-    NetDeviceContainer devices; // contains two nodes
+    NetDeviceContainer devices;
     devices = pointToPoint.Install(nodes);
 
-    InternetStackHelper stack; 
-    stack.Install(nodes); // install TCP/IP stack on nodes
+    InternetStackHelper stack;
+    stack.Install(nodes);
 
     Ipv4AddressHelper address;
-    address.SetBase("10.1.1.0", "255.255.255.0"); // first 10.1.1.1, followed by 10.1.1.2
-    // The low level ns-3 system remembers all of the IP addresses allocated
-    // -> a fatal error if the same address is generated twice (hard to debug!)
+    address.SetBase("10.1.1.0", "255.255.255.0");
 
     Ipv4InterfaceContainer interfaces = address.Assign(devices);
 
-    // 2. Applications to generate traffic
-
-    // Set up a UDP echo server application on one of the nodes we have previously created
     UdpEchoServerHelper echoServer(9);
 
-    ApplicationContainer serverApps = echoServer.Install(nodes.Get(1)); // implicit conversion: Ptr<Node> -> NodeContainer
-    serverApps.Start(Seconds(1)); // explicit conversion: double -> Time
+    ApplicationContainer serverApps = echoServer.Install(nodes.Get(1));
+    serverApps.Start(Seconds(1));
     serverApps.Stop(Seconds(10));
 
-    // The first interface in the interfaces container corresponds to the IP address of the first node in the nodes container
     UdpEchoClientHelper echoClient(interfaces.GetAddress(1), 9);
     echoClient.SetAttribute("MaxPackets", UintegerValue(1));
     echoClient.SetAttribute("Interval", TimeValue(Seconds(1)));
@@ -72,10 +62,6 @@ main(int argc, char* argv[])
     clientApps.Start(Seconds(2));
     clientApps.Stop(Seconds(10));
 
-    // 3. Running the simulation
-
-    // The system will begin looking through the list of scheduled events and executing them
-    // Simulator::Stop(Seconds(11));
     Simulator::Run();
     Simulator::Destroy();
     return 0;
